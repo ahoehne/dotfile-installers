de461bffb58b390d02590d9b15ce70488615f11e
Andreas Höhne <info@webdesign-hoehne.de>
2024-03-31 14:25:25 +0200

    better git integration thanks to fish and arch wiki

--- dotfiles/.config/fish/config.fish
+++ dotfiles/.config/fish/config.fish
@@ -1,15 +1,29 @@
 function fish_prompt
-	set -l git_branch (git branch 2>/dev/null | sed -n '/\* /s///p')
 	set_color $fish_color_cwd
 	echo -n (prompt_pwd)
-	if test -n "$git_branch"
-		set_color normal
-		echo -n '{'
-		set_color purple
-		echo -n "$git_branch"
-		set_color normal
-		echo -n '}'
+	if not set -q __fish_git_prompt_show_informative_status
+		set -g __fish_git_prompt_show_informative_status 1
 	end
+	if not set -q __fish_git_prompt_color_branch
+		set -g __fish_git_prompt_color_branch brmagenta
+	end
+	if not set -q __fish_git_prompt_showupstream
+		set -g __fish_git_prompt_showupstream "informative"
+	end
+	if not set -q __fish_git_prompt_showdirtystate
+		set -g __fish_git_prompt_showdirtystate "yes"
+	end
+	if not set -q __fish_git_prompt_color_stagedstate
+		set -g __fish_git_prompt_color_stagedstate yellow
+	end
+	if not set -q __fish_git_prompt_color_invalidstate
+		set -g __fish_git_prompt_color_invalidstate red
+	end
+	if not set -q __fish_git_prompt_color_cleanstate
+		set -g __fish_git_prompt_color_cleanstate brgreen
+	end
+	set_color normal
+	echo -n (fish_git_prompt);
 	set_color normal
 	echo -n '$ '
 end
